f4649494293e4fb27e37d7a550b9d766ca609567
Marat Tlepberghenov <mur.marat@gmail.com>
2018-02-19 16:18:32 +0200

    Create a project Fury.

---
+++ desktop/src/com/fury/game/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.fury.game.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.fury.game.MainGameClass;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new MainGameClass(), config);
+	}
+}
